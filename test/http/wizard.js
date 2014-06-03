12bb47055edcf6597797c711788da28be2fe7790
Abiy Seifu <abiy@ripple.com>
2014-06-03 15:02:26 -0700

    [TEST] config property validation

--- test/http/wizard.js
+++ test/http/wizard.js
@@ -37,7 +37,6 @@ describe('set configuration profile', function(){
       });
   });
 
-
   it('should return error with no database_url', function(done){
     request(app)
       .post('/v1/config/wizard')
